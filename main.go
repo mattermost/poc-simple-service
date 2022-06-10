deb63bca664afb7277faa3d8b52ce548032d8de1
Mustafa Kara <mustafa.kara@mattermost.com>
2022-06-10 14:37:35 +0300

    Create simple rest project
    Signed-off-by: Mustafa Kara <mustafa.kara@mattermost.com>

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type welcomeResponse struct {
+	Message string `json:"message"`
+}
+
+func hello(c *gin.Context) {
+	var response = welcomeResponse{Message: "Hello!"}
+	c.IndentedJSON(http.StatusOK, response)
+}
+
+func setupServer() *gin.Engine {
+	router := gin.Default()
+	router.GET("/hello", hello)
+	return router
+}
+
+func main() {
+	if err := setupServer().Run(); err != nil {
+		log.Fatalf("Error while starting server!")
+	}
+
+}
